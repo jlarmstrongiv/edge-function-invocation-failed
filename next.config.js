b1707c04ebc93cefe6b8cbf36b04f74fc583b0f1
John L. Armstrong IV <jlarmstrongiv@gmail.com>
2021-12-13 01:31:37 -0500

    ci: reproduce EDGE_FUNCTION_INVOCATION_FAILED

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+  optimizeFonts: false,
+  staticPageGenerationTimeout: 60,
+  swcMinify: true,
+  experimental: {
+    pageEnv: true,
+    scrollRestoration: true,
+    externalDir: true,
+    reactRoot: "concurrent",
+    concurrentFeatures: true,
+  },
+};
